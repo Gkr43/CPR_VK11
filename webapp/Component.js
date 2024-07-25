dc649e1bd67e3123f1c9fe91b7a9b48421b59992
Prasad Dixit <prasad.dixit@zencon.co.in>
2024-07-25 06:59:18 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("cprvk11.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
